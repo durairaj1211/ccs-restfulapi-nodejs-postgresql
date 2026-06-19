0d3f07e3bfc062a6f57e4b37a22a524494027088
DurairajRepo <durairaj1211@gmail.com>
2026-06-19 20:09:40 +0530

    Validation concept include

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,15 +4,16 @@ import dotenv from "dotenv";
 //import pool from "./config/db.js";
 
 import { createUser, getAllUsers, getUserById, updateUser, deleteUser } from "../controllers/userController.js";
+import validateUser from "../middlewares/inputValidator.js";
 
 const router = express.Router();
 
 dotenv.config();
 
-router.post("/user", createUser);
+router.post("/user", validateUser, createUser);
 router.get("/user", getAllUsers);
 router.get("/user/:id", getUserById);
-router.put("/user/:id", updateUser);
+router.put("/user/:id", validateUser, updateUser);
 router.delete("/user/:id", deleteUser);
 
 export default router;
